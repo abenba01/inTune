c9342ff247bbfadb5b7359de1c32cecf69dda55d
Alexandra Benbadis <benbadass95@Alexandras-MacBook-Pro-2.local>
2015-11-20 13:07:30 -0500

    AB did redirecting

---
+++ frontend.js
@@ -0,0 +1,8 @@
+function getstarted(){
+
+    var btn = document.getElementById('getstarted_button');
+		btn.addEventListener('click', function() {
+		  document.location.href = '/main.html';
+		});
+
+};
