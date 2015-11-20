57ff482b3836d38351f8f83387c9b7809226d68e
Alexandra Benbadis <benbadass95@Alexandras-MacBook-Pro-2.local>
2015-11-20 12:45:23 -0500

    added server things-AB

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 //Hi this is our server
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+
+// landing page
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+
+// listen page
+app.get('/main.html', function(request, response) {
+	response.sendFile(__dirname + "/main.html");
+});
 
 	var myLat = 0;
 	var myLng = 0;
@@ -50,3 +63,6 @@
    			}
    		});
 	};
+
+
+	app.listen(process.env.PORT || 5000);
