6473e2a03d0b99769eca788da4297aa716dc5510
Daniel Navon <dnavon2557@gmail.com>
2015-11-26 14:00:49 -0500

    DN added text to bubbles that is centered

--- bubbles.js
+++ bubbles.js
@@ -1,10 +1,16 @@
+
+function addBubble(name, x_start, y_start) {
 var canvas = document.getElementById('bubble_cvs');
 var ctx = canvas.getContext('2d');
 var raf;
 
+
+canvas.width = window.innerWidth; 
+
 var ball = {
-  x: 100,
-  y: 100,
+  text: name,
+  x: x_start,
+  y: y_start,
   vx: 5,
   vy: 2,
   radius: 25,
@@ -15,6 +21,11 @@ var ball = {
     ctx.closePath();
     ctx.fillStyle = this.color;
     ctx.fill();
+    var textSize = (this.radius*2 / this.text.length) *2;
+    ctx.fillStyle = "red";
+    ctx.font =  textSize + "px Arial";
+    ctx.textAlign = "center";
+    ctx.fillText(this.text, this.x, this.y + textSize/4);
   }
 };
 
@@ -24,6 +35,8 @@ function draw() {
   ball.x += ball.vx;
   ball.y += ball.vy;
   raf = window.requestAnimationFrame(draw);
+  ball.vy *= .99;
+ball.vy += .25;
   if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
   ball.vy = -ball.vy;
 }
@@ -40,4 +53,7 @@ canvas.addEventListener("mouseout",function(e){
   window.cancelAnimationFrame(raf);
 });
 
-ball.draw();
+ball.draw(); 
+}
+
+addBubble("hello", 100, 100);
