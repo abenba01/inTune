76e95d2d6d7f2d510dc7c591995ad97d48dce47f
Daniel Navon <dnavon2557@gmail.com>
2015-11-27 01:17:30 -0500

    Bubbles now appear in footer on click and pop in

--- bubbles.js
+++ bubbles.js
@@ -1,10 +1,20 @@
 
 var canvas = document.getElementById('bubble_cvs');
+var footer_canvas = document.getElementById('footer_cvs');
+var footer = footer_canvas.getContext('2d');
+var footer_height = document.getElementById("bubble_footer").getBoundingClientRect().height;
+var header_height = document.getElementById("header").getBoundingClientRect().height;
 canvas.width = window.innerWidth; 
-var ctx = canvas.getContext('2d');
+canvas.height = window.innerHeight - footer_height - header_height;
+//footer_canvas.style.height = "117px";
+var footer_rect = footer_canvas.getBoundingClientRect();
+footer_canvas.height = footer_rect.height;
+footer_canvas.width = footer_rect.width;
+var main_ctx = canvas.getContext('2d');
 var rect = canvas.getBoundingClientRect();
 var raf;
 var bubbles = new Object;
+var footer_bubbles = new Object;
 
 var band_names = [
 "Nickleback", "The Strokes", "The Killers", "Father John Misty", "Future", 
@@ -26,7 +36,14 @@ function addBubble(name, x_start, y_start) {
 	radius: 50,
 	color: "#71D1EA",
 	pop: false,
+	in_footer: false,
 	draw: function() {
+		var ctx;
+		if (!this.in_footer) {
+			ctx = main_ctx;
+		} else {
+			ctx = footer;
+		}
 		  ctx.beginPath();
 		  ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
 		  ctx.closePath();
@@ -50,12 +67,14 @@ function addBubble(name, x_start, y_start) {
 	return ball;
 }
 function draw() {
-	ctx.clearRect(0,0, canvas.width, canvas.height);
+	main_ctx.clearRect(0,0, canvas.width, canvas.height);
+	footer.clearRect(0,0, footer_canvas.width, footer_canvas.height);
 	for (var key in bubbles) {
 		bubbles[key].draw();
 		var bubble = bubbles[key];
 		if(bubble.pop == true) {
 			if (bubble.radius <= 3){
+				push_to_footer(key);
 				delete bubbles[key];
 				continue;
 			} else {
@@ -73,13 +92,25 @@ function draw() {
 			bubble.vx = -bubble.vx;
 		}	
 	}
+	for (var key in footer_bubbles) {
+		var bubble = footer_bubbles[key];
+		bubble.draw();
+		if(bubble.radius < 50 && bubble.pop) {
+			bubble.radius *= 2;
+		}
+	}
 	raf = window.requestAnimationFrame(draw);
 }
 
 
+function push_to_footer(key) {
+	footer_bubbles[key] = addBubble(key, 100 + 100*Object.keys(footer_bubbles).length, footer_rect.height/2);
+	footer_bubbles[key].in_footer = true;
+	footer_bubbles[key].pop = true;
+	footer_bubbles[key].radius = 3.125;
+	footer_bubbles[key].draw();
 
-
-
+}
 
 
 
@@ -132,6 +163,9 @@ canvas.addEventListener("click", function(e) {
 		}
 	}
 });
+
+
+
 /*
 function pop_bubble(key) {
 	var bubble = bubbles[key];
