da7c5b34bfb40b66139f7b4f6f46d58d61fbf87a
Daniel Navon <dnavon2557@gmail.com>
2015-12-12 17:26:09 -0500

    revereted to smome old copy this could probably work idk

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/main.html', function (request, response) {
 
 app.post('/savePlaylist', function (request, response) {
 	response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	//console.log(request.body);
 	//console.log("test");
 	songs = request.body;
