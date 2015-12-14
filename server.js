b8c8cef29ccd0dad89cd55cc86d7d7dc62749051
Daniel Navon <dnavon2557@gmail.com>
2015-12-13 22:04:23 -0500

    altered log statements for server

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/getPlaylist', function (request, response) {
 			coll.find({"_id":{"$oid": reqId}}, function(err, results) {
 				if (!err) {
 					console.log("found");
-					console.log(results.body.frame);
+					console.log(results);
 					response.send(results.frame);
 				} 
 			});
